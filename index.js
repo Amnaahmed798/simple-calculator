b247dd688216880c2280b66d4f0783df57304bac
Amna Ahmed <160534192+Amnaahmed798@users.noreply.github.com>
2024-03-30 14:32:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+console.log("\n\twelcome to \'amna\' CLI simple calculator\n");
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter your first number", name: "firstnumber", type: "number" },
+    { message: "enter your second number", name: "secondnumber", type: "number" },
+    { message: "select one of the operator to perform action", name: "operator", type: "list", choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "divison") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    "please select valid operator";
+}
+;
